a91280aa0f720ad4769da7b5541494995c420fce
Tranthi02 <Tranthyy02@gmail.com>
2024-08-16 13:37:38 +0700

    [feat] thêm js scroll to top

--- MXH_01/assest/js/main.js
+++ MXH_01/assest/js/main.js
@@ -1,7 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector(".menu-toggle").addEventListener("click", function () {
-      document.querySelector(".menu-items ul.nav").classList.toggle("show");
-      document.querySelector(".wrapper").classList.toggle("activeBg");
-    });
+  document.querySelector(".menu-toggle").addEventListener("click", function () {
+    document.querySelector(".menu-items ul.nav").classList.toggle("show");
+    document.querySelector(".wrapper").classList.toggle("activeBg");
   });
-  
+});
+
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+scrollToTopBtn.onclick = function () {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
